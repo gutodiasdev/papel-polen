2eb5bde6310b47249d56852e79575ded17ca189e
gutodiasux <gutodiasux@gmail.com>
2023-08-08 10:39:22 -0300

    feat: add only section

---
+++ src/app/development/page.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen min-w-full">
+      <section className="bg-[url('/home_hero_background.png')] bg-no-repeat bg-right-top min-h-screen ">
+        <div className="flex  gap-x-8 items-center">
+          <img src='/logo_polen_natural.png' alt='Logo Polen Natural' />
+          <nav className="flex gap-x-4">
+            <Link href='#'>
+              Características
+            </Link>
+            <Link href='#'>
+              Papel Pólen
+            </Link>
+            <Link href='#'>
+              Sustentabilidade
+            </Link>
+            <Link href='#'>
+              Case
+            </Link>
+          </nav>
+        </div>
+        <div>
+          <h1>
+            Um papel que valoriza o prazer da leitura
+          </h1>
+          <p>
+            O papel Pólen® Natural foi criado especialmente para livros, com tratamento diferenciado que resulta em
+            tonalidade off white, menos reflexo de luz e uma leitura mais agradável e prolongada. Ele oferece uma
+            experiência sensorial confortável, macio ao toque, proporcionando um belo acabamento ao produto final.
+          </p>
+          <a href="#">Quero usar papel polen</a>
+        </div>
+      </section>
+      <section>
+        <h2>Video section</h2>
+      </section>
+      <section>
+        <div>
+          <h2>
+            Características do Papel Pólen®
+          </h2>
+          <p>
+            Papel Pólen® Natural é sem adição de OBA (Optical Brighteners Agent). Sua produção é feita com uma 
+            tecnologia que dispensa esses tipos de aditivos e reduza quantidade de química no produto final.
+          </p>
+        </div>
+        <div>
+            <div>
+              <h3>Naturalmente melhor para as editoras</h3>
+              <p>O Pólen Natural é produzido de forma otimizada e oferece melhor desempenho na fabricação de livros.</p>
+            </div>
+            <div>
+              <h3>Naturalmente melhor para leitores</h3>
+              <p>Leitura mais confortável para os olhos.</p>
+            </div>
+            <div>
+              <h3>Naturalmente melhor para escritores</h3>
+              <p>Toda a sofisticação de Pólen, com corpo e acabamento de superfície para valorizar uma boa história.</p>
+            </div>
+        </div>
+      </section>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,35 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen min-w-full">
+      <section className="min-h-screen bg-[#231f20] rounded-tl-[32rem] md:rounded-tl-none lg:rounded-bl-[32rem] md:grid md:grid-cols-2 items-center  pt-8 pb-16">
+        <div className="flex w-full justify-center">
+          <img src="/illustration_01.svg" alt="Logo Polen Natural" className="w-4/5" />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="text-white pl-4 pr-8 flex flex-col gap-y-8 pt-4 md:pt-0">
+          <p>
+            Para neutralizar as emissões de carbono, a Suzano contou com apoio de uma
+            consultoria especializada para mapear e calcular a pegada de carbono de todo o ciclo
+            de vida dos livros, desde a plantação do eucalipto até o descarte/reciclagem de cada
+            obra. O cálculo foi realizado de forma individualizada para cada livro e para
+            neutralizar as emissões serão utilizados créditos de carbono da Suzano, oriundos de
+            projetos certificados e verificados por auditoria externa.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p>
+            Essa é uma iniciativa alinhada com a estratégia de negócios da Suzano. Focada em
+            soluções baseadas na natureza, a companhia busca conservar a biodiversidade e
+            regular o ciclo hidrológico por meio de suas áreas de plantio e conservação, ao
+            mesmo tempo que oferece soluções sustentáveis aos clientes e contribui para
+            reduzir as emissões de carbono na cadeia de valor.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
+          <p>
+            Mas esse não é um trabalho de apenas uma empresa. Contamos com o apoio
+            de diversos atores nessa jornada, incluindo fornecedores e clientes. Nosso
+            propósito é renovar a vida a partir da árvore e nós contamos com você. Faça
+            parte desse movimento.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </div>
+      </section>
     </main>
   )
 }
