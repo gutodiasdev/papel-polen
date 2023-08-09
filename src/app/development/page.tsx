7446cb810f34e8ca26380e182fd6f55e1dc89990
gutodiasux <gutodiasux@gmail.com>
2023-08-08 21:55:43 -0300

    adjust md responsive

--- src/app/development/page.tsx
+++ src/app/development/page.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen min-w-full">
-      <section className="bg-[url('/home_hero_background.png')] bg-no-repeat bg-[right_-24rem_top_-6rem]">
-        <div className="flex flex-col gap-x-8 gap-y-6 items-center py-8 px-3">
+      <section className="bg-[url('/home_hero_background.svg')] bg-no-repeat bg-[right_-24rem_top_-6rem]
+      md:bg-[right_-8rem_top] md:bg-contain ">
+        <div className="flex flex-col md:flex-row gap-x-8 gap-y-6 items-center py-8 px-3 md:px-6">
           <img src='/logo_polen_natural.png' alt='Logo Polen Natural' />
           <nav className="flex gap-x-4 text-sm">
             <Link href='#'>
@@ -21,7 +22,7 @@ export default function Home() {
             </Link>
           </nav>
         </div>
-        <div className="px-3 py-8">
+        <div className="px-3 py-8 md:w-1/2 md:px-6">
           <h1 className="text-5xl">
             Um papel que valoriza o prazer da leitura
           </h1>
@@ -36,14 +37,14 @@ export default function Home() {
           </button>
         </div>
       </section>
-      <section>
+      <section className="md:py-16">
         <div className='grid place-items-center relative overflow-hidden w-full pt-[56.25%]'>
           <iframe className='absolute top-0 left-0 bottom-0 right-0 w-full h-full' src="https://www.youtube.com/embed/PMwDeoh0LM4" title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
       </section>
-      <section className="px-3 py-8">
-        <div>
+      <section className="px-3 md:px-6 py-8 md:grid md:grid-cols-2 md:gap-x-4 md:overflow-hidden md:items-center">
+        <div className="md:-mt-12">
           <h2 className="text-5xl">
             Características do Papel Pólen®
           </h2>
@@ -52,25 +53,26 @@ export default function Home() {
             tecnologia que dispensa esses tipos de aditivos e reduza quantidade de química no produto final.
           </p>
         </div>
-        <div className="py-8">
-          <div className="bg-[url('/yellow_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px]">
+        <div className="py-8 md:flex md:gap-x-4">
+          <div className="bg-[url('/yellow_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px] md:min-w-[300px]">
             <img src="/apartment.svg" alt="Pine" className="w-[24px] h-[24px]" />
             <h3 className="text-[#231f20] text-2xl py-4">Naturalmente melhor para as editoras</h3>
             <p className="text-xl">O Pólen Natural é produzido de forma otimizada e oferece melhor desempenho na fabricação de livros.</p>
           </div>
-          <div className="bg-[url('/yellow_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px]">
+          <div className="bg-[url('/yellow_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px] md:min-w-[300px]">
             <img src="/description.svg" alt="Pine" className="w-[24px] h-[24px]" />
             <h3 className="text-[#231f20] text-2xl py-4">Naturalmente melhor para leitores</h3>
             <p className="text-xl">Leitura mais confortável para os olhos.</p>
           </div>
-          <div className="bg-[url('/yellow_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px]">
+          <div className="bg-[url('/yellow_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px] md:min-w-[300px]">
             <img src="/edit_note.svg" alt="Pine" className="w-[24px] h-[24px]" />
             <h3 className="text-[#231f20] text-2xl py-4">Naturalmente melhor para escritores</h3>
             <p className="text-xl">Toda a sofisticação de Pólen, com corpo e acabamento de superfície para valorizar uma boa história.</p>
           </div>
         </div>
       </section>
-      <section className="px-3 py-8">
+      <section className="px-3 md:px-6 py-8 bg-[url('/why_use_background.svg')] bg-no-repeat bg-[right_-24rem_top_-6rem]
+      md:bg-[left_top] bg-contain md:grid md:grid-cols-2">
         <div></div>
         <div>
           <h2 className="text-5xl">
@@ -98,20 +100,21 @@ export default function Home() {
               <p>Gramatura 70g e 80g</p>
             </div>
           </div>
-        </div>
-        <button type="submit" className='bg-[#F7941D] p-2 rounded-xl w-full text-[#FFF7E0] px-8 mt-8
+          <button type="submit" className='bg-[#F7941D] p-2 rounded-xl w-full text-[#FFF7E0] px-8 mt-8
             hover:bg-[#231f20] hover:text-[#F7941D] transition ease-out'>
-          Quero usar papel pólen
-        </button>
+            Quero usar papel pólen
+          </button>
+        </div>
       </section>
-      <section className="bg-[url('/background.svg')] grid grid-cols-1 px-3 py-8">
+      <section className="bg-[url('/background.svg')] grid grid-cols-1 px-3 md:px-6 py-8 md:py-16
+      md:grid md:grid-cols-2 md:overflow-hidden md:items-center">
         <div>
           <h2 className="text-5xl">
             Mais próximo da natureza
           </h2>
         </div>
-        <div className="grid grid-cols-1 gap-y-4 py-8">
-          <div className="bg-[url('/dark_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px]">
+        <div className="grid grid-cols-1 gap-y-4 py-8 md:flex">
+          <div className="bg-[url('/dark_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px] md:min-w-[300px]">
             <img src="/pine_tree.svg" alt="Pine" className="w-[24px] h-[24px]" />
             <p className="mt-8 text-lg">
               Visando fornecer soluções sustentáveis, a nossa empresa se dedica ao plantio e cultivo de árvores, as
@@ -119,14 +122,14 @@ export default function Home() {
               de mais de 2 bilhões de pessoas.
             </p>
           </div>
-          <div className="bg-[url('/dark_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px]">
+          <div className="bg-[url('/dark_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px] md:min-w-[300px]">
             <img src="/nature_tree.svg" alt="Pine" className="w-[24px] h-[24px]" />
             <p className="mt-8 text-lg">
               Por meio do desenvolvimento de uma ampla gama de materiais, utilizando árvores cultivadas por nós, nos
               comprometemos a tornar os produtos disponíveis nas lojas e prateleiras ao redor do mundo mais sustentáveis.
             </p>
           </div>
-          <div className="bg-[url('/dark_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px]">
+          <div className="bg-[url('/dark_tag.svg')] bg-no-repeat bg-contain text-[#FFF7E0] p-8 min-h-[400px] md:min-w-[300px]">
             <img src="/two_pine_trees.svg" alt="Pine" className="w-[24px] h-[24px]" />
             <p className="mt-8 text-lg">
               A empresa tem a sustentabilidade como um pilar essencial em sua trajetória e busca conscientizar e
@@ -135,9 +138,10 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='py-8 px-3 grid gap-y-8'>
+      <section className="py-8 md:py-16 px-3 md:px-6 grid gap-y-8 bg-[url('/closer_to_nature.svg')] bg-no-repeat bg-[right_-24rem_top_-6rem]
+      md:bg-[right_top] md:bg-contain md:grid md:grid-cols-2">
         <div className="grid gap-y-4">
-          <h2 className="text-3xl">
+          <h2 className="text-5xl">
             Suzano ajuda o Mundo a respirar melhor
           </h2>
           <p>
@@ -152,9 +156,10 @@ export default function Home() {
           <img src="/edicao_carbono_zero.svg" alt="Logo Polen Natural" className="w-3/5" />
         </div>
       </section>
-      <section className="py-8 px-3 grid">
+      <section className="py-8 md:py-16 px-3 md:px-6 grid bg-[url('/background_tree.svg')] bg-no-repeat bg-[right_-24rem_top_-6rem]
+      md:bg-[right_top] md:grid md:grid-cols-2">
         <div>
-          <div className='flex justify-center gap-x-4'>
+          <div className='flex justify-center md:justify-start gap-x-4'>
             <img src="/logo_polen_and_points.svg" alt="Logo Polen Natural" className="w-[160px] h-[64px]" />
           </div>
           <form action="" className='flex flex-col gap-y-4 max-w-[320px]  md:max-w-[480px] lg:max-w-[640px] xl:max-w-[880px] mx-auto my-12'>
@@ -166,7 +171,7 @@ export default function Home() {
               Enviar
             </button>
           </form>
-          <div className='grid md:grid-cols-1 lg:grid-cols-3 max-w-[320px] md:max-w-[480px] lg:max-w-[640px] xl:max-w-[880px] mx-auto 
+          <div className='grid md:grid-cols-3 lg:grid-cols-3 max-w-[320px] md:max-w-[480px] lg:max-w-[640px] xl:max-w-[880px] mx-auto 
         gap-y-4 md:gap-y-8'>
             <div>
               <div className='flex gap-x-2 justify-center items-center'>
