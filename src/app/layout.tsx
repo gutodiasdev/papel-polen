07e6129ad5f88f85300292c14998182187923656
gutodiasux <gutodiasux@gmail.com>
2023-08-09 19:14:57 -0300

    adjust prisma and meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,11 +40,33 @@ export default function RootLayout({
             `,
             }}
           />
+          <Script
+            id='google-analytics'
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '238816705230083');
+              fbq('track', 'PageView');
+            `,
+            }}
+          />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5MBKH4W" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            }}
+          />
         </head>
         <body className={`bg-[#fff7e0]`} style={{ fontFamily: minionPro.style.fontFamily }}>
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5MBKH4W" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+              __html: `<img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=238816705230083&ev=PageView&noscript=1"/>`,
             }}
           />
           {children}
